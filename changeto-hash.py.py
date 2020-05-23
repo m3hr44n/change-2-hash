cd53e21d2d917658653fdc5fb87e597c221248df
m3hr44n <65813343+m3hr44n@users.noreply.github.com>
2020-05-23 10:25:15 -0700

    Add files via upload

---
+++ changeto-hash.py.py
@@ -0,0 +1,22 @@
+#python project
+#mehraan kiya
+#roshanamooz
+
+import hashlib, bcrypt
+
+password = input("Input the password to hash\n>")
+print("\nSHA1:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.sha1(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nMD5:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.md5(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nBCRYPT:\n")
+for i in range(3):
+    hashed = bcrypt.hashpw(setpass, bcrypt.gensalt(10))
